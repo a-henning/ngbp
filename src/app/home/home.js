41410e4a0ab059276725ccec9789961ef825cfcf
Arthur Henning <h_arthur_12@yahoo.com>
2014-02-25 16:00:32 +0200

    Added Accordion and Carousel, commented them out because of bad style.

--- src/app/home/home.js
+++ src/app/home/home.js
@@ -74,5 +74,50 @@ angular.module( 'ngBoilerplate.home', [
         $scope.alerts.splice(index, 1);
     };
 
-});
+})
+
+/**
+ * Accordion demo controller.
+ */
+.controller('AccordionDemoCtrl', function AccordionDemoCtrl($scope) {
+    $scope.oneAtATime = true;
+
+    $scope.groups = [
+        {
+            title: "Dynamic Group Header - 1",
+            content: "Dynamic Group Body - 1"
+        },
+        {
+            title: "Dynamic Group Header - 2",
+            content: "Dynamic Group Body - 2"
+        }
+    ];
+
+    $scope.items = ['Item 1', 'Item 2', 'Item 3'];
+
+    $scope.addItem = function() {
+        var newItemNo = $scope.items.length + 1;
+        $scope.items.push('Item ' + newItemNo);
+    };
+})
+
+/**
+ * Carousel demo
+ */
+.controller('CarouselDemoCtrl', function CarouselDemoCtrl($scope) {
+    $scope.myInterval = 5000;
+    var slides = $scope.slides = [];
+    $scope.addSlide = function() {
+        var newWidth = 600 + slides.length;
+        slides.push({
+            image: 'http://placekitten.com/' + newWidth + '/300',
+            text: ['More','Extra','Lots of','Surplus'][slides.length % 4] + ' ' +
+                ['Cats', 'Kittys', 'Felines', 'Cutes'][slides.length % 4]
+        });
+    };
+    for (var i=0; i<4; i++) {
+        $scope.addSlide();
+    }
+})
+;
 
