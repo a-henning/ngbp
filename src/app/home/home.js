a93f991ecc6c1963557d7204c67f02fcaecf346f
Arthur Henning <h_arthur_12@yahoo.com>
2014-02-25 12:28:07 +0200

    Added bootstrap ui toggle button demo.

--- src/app/home/home.js
+++ src/app/home/home.js
@@ -43,3 +43,16 @@ angular.module( 'ngBoilerplate.home', [
 
 ;
 
+var ButtonsCtrl = function ($scope) {
+
+    $scope.singleModel = 1;
+
+    $scope.radioModel = 'Middle';
+
+    $scope.checkModel = {
+        left: false,
+        middle: true,
+        right: false
+    };
+};
+
